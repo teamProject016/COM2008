608fbe336dbbcdaced99d3218cb875a80a7f5500
teamProject016 <74458243+teamProject016@users.noreply.github.com>
2020-12-02 01:11:25 +0000

    Update and rename TeacherAndModules.sql to Modules.sql

--- Modules.sql
+++ Modules.sql
@@ -1,4 +1,4 @@
-CREATE TABLE module_info(
+CREATE TABLE Modules(
   register_num INT(20) NOT NULL,
   module_name TEXT          NOT NULL,
   module_code VARCHAR(100)     NOT NULL,
@@ -11,7 +11,7 @@ CREATE TABLE module_info(
 
 
 -- add or update grades
-ALTER TABLE module_info(
+ALTER TABLE Modules(
 ADD grades FLOAT(100) NOT NULL,
     register_num INT(20) NOT NULL,
     module_name TEXT          NOT NULL,
@@ -23,7 +23,7 @@ ADD grades FLOAT(100) NOT NULL,
     PRIMARY KEY (module_code),
 
 );
-ALTER TABLE module_info(
+ALTER TABLE Modules(
 ADD resit_exam FLOAT(100),
     register_num INT(20) NOT NULL,
     module_name TEXT          NOT NULL,
@@ -35,7 +35,7 @@ ADD resit_exam FLOAT(100),
     PRIMARY KEY (module_code),
 
 );
-ALTER TABLE module_info(
+ALTER TABLE Modules(
 ADD repeat_year FLOAT(100),
     register_num INT(20) NOT NULL,
     module_name TEXT          NOT NULL,
@@ -47,7 +47,7 @@ ADD repeat_year FLOAT(100),
     PRIMARY KEY (module_code),
 );
 
-ALTER TABLE module_info(
+ALTER TABLE Modules(
 DROP  register_num INT(20) NOT NULL,
       module_name TEXT          NOT NULL,
       module_code VARCHAR(100)     NOT NULL,
@@ -61,11 +61,11 @@ DROP  register_num INT(20) NOT NULL,
 
 -- calculate the mean grade for a period of study
 SELECT ID,NAME,mean_grade=(AVG(grades))
-FROM module_info
+FROM Modules
 
 -- check the module credits 120 for undergraduate 180 for postgraduates
 SELECT level_study,module_code,all_credits=SUM(credits),
-FROM module_info
+FROM Modules
 
 
 
@@ -75,63 +75,63 @@ CASE
     WHEN grades >= 40 THEN 'Pass'
     WHEN grades < 40 THEN 'Need a resit'
 END AS module_status
-FROM module_info;
+FROM Modules;
 
 -- student may progress or not
-UPDATE module_info
+UPDATE Modules
 SET module_status='pass'
 WHERE grades >= 40 AND level_study <=2;
-INSERT INTO module_info (final_result)
+INSERT INTO Modules (final_result)
 VALUES ('progrees to next level');
 
-UPDATE module_info
+UPDATE Modules
 SET module_status='pass'
 WHERE grades >= 40 AND level_study >=3;
-INSERT INTO module_info (final_result)
+INSERT INTO Modules (final_result)
 VALUES ('graduate');
 
-UPDATE module_info
+UPDATE Modules
 SET module_status='fail'
 WHERE grades < 40;
-INSERT INTO module_info (final_result)
+INSERT INTO Modules(final_result)
 VALUES ('fail to progress');
 
 -- resit and repeat year
 
 
-UPDATE module_info
+UPDATE Modules
 SET resit_exam = 40
 WHERE resit_exam >= 40 AND level_study <= 2;
-INSERT INTO module_info (final_result)
+INSERT INTO Modules (final_result)
 VALUES ('progress to next level');
 
-UPDATE module_info
+UPDATE Modules
 SET resit_exam = 50
 WHERE resit_exam >= 50 AND level_study >= 3;
-INSERT INTO module_info (final_result)
+INSERT INTO Modules (final_result)
 VALUES ('graduate');
 
-UPDATE module_info
+UPDATE Modules
 SET repeat_year = 40
 WHERE repeat_year >= 40 AND level_study <= 2;
-INSERT INTO module_info (final_result)
+INSERT INTO Modules(final_result)
 VALUES ('progress to next level');
 
-UPDATE module_info
+UPDATE Modules
 SET repeat_year = 50
 WHERE repeat_year >= 50 AND level_study >= 3;
-INSERT INTO module_info (final_result)
+INSERT INTO Modules (final_result)
 VALUES ('graduate');
 
-UPDATE module_info
+UPDATE Modules
 SET module_status='fail'
 WHERE repeat_year < 40;
-INSERT INTO module_info (final_result)
+INSERT INTO Modules (final_result)
 VALUES ('fail to progress');
 
 
 -- determine the degree class
-SELECT module_info.module_code, (AVG(grades)) AS TotalMark,degree_class,
+SELECT Modules.module_code, (AVG(grades)) AS TotalMark,degree_class,
     CASE WHEN (AVG(grades)) < 39.5 IN ('1-year MSc','BSc,BEng degrees','MComp,MEng degrees')
     THEN 'fail'
     WHEN (AVG(grades)) < 44.4 AND (AVG(grades)) > 39.5 IN ('1-year MSc','MComp,MEng degrees')
@@ -156,16 +156,16 @@ SELECT module_info.module_code, (AVG(grades)) AS TotalMark,degree_class,
     WHEN (AVG(grades)) >= 69.5 IN ('BSc,BEng degrees','MComp,MEng degrees')
     THEN 'first class'
     END AS DEGREE_CLASS
-    FROM module_info;
+    FROM Modules;
 
 -- student and modules
 SELECT *
 FROM Students
-INNER JOIN module_info ON Students.register_num = module_info.register_num;
+INNER JOIN Modules ON Students.register_num = Modules.register_num;
 
 
 -- view the status of all students
 SELECT *
 FROM student
-FULL OUTER JOIN module_info ON Students.register_num = module_info.register_num
+FULL OUTER JOIN Modules ON Students.register_num = Modules.register_num
 ORDER BY Students.surname;
